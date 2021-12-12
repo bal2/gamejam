73d9db779405bcbc208405a6aab9d1b3701f7d98
Håkon Knudsen <hafknudsen@outlook.com>
2021-12-12 11:25:08 +0100

    Arrow keys movement

--- static/game.js
+++ static/game.js
@@ -9,15 +9,19 @@ let movement = {
 document.addEventListener('keydown', (event) => {
     switch (event.keyCode) {
         case 65: // A
+        case 37: // ->
             movement.left = true;
             break;
         case 87: // W
+        case 38: // ^
             movement.up = true;
             break;
         case 68: // D
+        case 39: // <-
             movement.right = true;
             break;
         case 83: // S
+        case 40: // v
             movement.down = true;
             break;
     }
@@ -25,15 +29,19 @@ document.addEventListener('keydown', (event) => {
 document.addEventListener('keyup', (event) => {
     switch (event.keyCode) {
         case 65: // A
+        case 37: // ->
             movement.left = false;
             break;
         case 87: // W
+        case 38: // ^
             movement.up = false;
             break;
         case 68: // D
+        case 39: // <-
             movement.right = false;
             break;
         case 83: // S
+        case 40: // v
             movement.down = false;
             break;
     }
